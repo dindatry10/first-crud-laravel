9aa6f9d4a44599d1617efe76c2c3348050261bb4
dindatry10 <dindatry10@gmail.com>
2022-10-05 11:31:01 +0700

    commit kedua

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index'])->name('index.edit');;
+Route::get('/', [IndexController::class, 'index'])->name('index.edit');
 Route::get('/edit/{id_user}', [IndexController::class, 'edit'])->name('user.edit');
 Route::post('/update/{id_user}', [IndexController::class, 'update'])->name('user.update');
 Route::get('/add', [IndexController::class, 'add'])->name('user.add');
